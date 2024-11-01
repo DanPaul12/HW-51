f56e2f25c3f2d31f7a4d3cdd3f42f9b7374ecb37
DanPaul12 <dan.paul.schechter@gmail.com>
2024-10-31 18:01:19 -0700

    hw51

--- marvel-react/src/App.jsx
+++ marvel-react/src/App.jsx
@@ -1,16 +1,27 @@
 import axios from 'axios'
 import './App.css'
+import { useEffect } from 'react'
 
 function App() {
 
+  const [character, setCharacter] = useState(null)
+
+  useEffect(() => {
+    getCharacter('nightcrawler')
+  }, [])
+
   const getCharacter = async (marvelname) =>{
-    const response = await axios.get(`https://gateway.marvel.com:443/v1/public/characters?name=${marvelname}&limit=1&ts=1&apikey=eed80ca7412bb4d9fb5325f1f50453d0&hash=3104267868a886eae2f5db79822449d8`)
-    const character = response.data 
-    return character
+    try {
+      const response = await axios.get(`https://gateway.marvel.com:443/v1/public/characters?name=${marvelname}&limit=1&ts=1&apikey=eed80ca7412bb4d9fb5325f1f50453d0&hash=3104267868a886eae2f5db79822449d8`)
+      setCharacter(response.data)
+      }
+    catch (error){
+      console.error(error)
+    }
   }
   return (
     <div id='container'>
-      {getCharacter('nightcrawler')}
+      <img src= {character.data.results[0].thumbnail.path} />
     </div>
   )
 }
