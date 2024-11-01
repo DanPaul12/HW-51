d41ed571ffe18f6a0bd667823f86aee21268573e
DanPaul12 <dan.paul.schechter@gmail.com>
2024-11-01 08:55:45 -0700

    hw51

--- marvel-react/src/App.jsx
+++ marvel-react/src/App.jsx
@@ -1,36 +1,17 @@
 import axios from 'axios'
 import './App.css'
 import { useState, useEffect } from 'react'
+import CharacterList from './components/CharacterList'
 
 function App() {
 
-  const [characters, setCharacters] = useState('')
-
-  useEffect(() => {
-    getCharacters()
-  }, [])
-
-  const getCharacters = async () => {
-    try {
-      const response = await axios.get(`https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=eed80ca7412bb4d9fb5325f1f50453d0&hash=3104267868a886eae2f5db79822449d8`)
-      setCharacters(response.data)
-      }
-    catch (error){
-      console.error('error:', error)
-    }
-  }
-
-  if (characters !== ''){     //what other way to do this? why does it console log 4 times?
-  return (                    //why so long to load
+ 
+                                
+  return (                    
     <div id='container'> 
-      <ul id='list1'>
-        {characters.data.results.map((character)=> 
-        <li key={character.id}> <img src={character.thumbnail.path+'.jpg'} />
-        <p>{character.name}</p></li>
-        )}
-      </ul>
+        <CharacterList />
     </div>
   )}
-}
+
 
 export default App
---
+++ marvel-react/src/components/CharacterList.jsx
@@ -0,0 +1,35 @@
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+
+function CharacterList() {
+
+  const [characters, setCharacters] = useState('')
+
+  useEffect(() => {
+    getCharacters()
+  }, [])
+
+  const getCharacters = async () => {
+    try {
+      const response = await axios.get(`https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=eed80ca7412bb4d9fb5325f1f50453d0&hash=3104267868a886eae2f5db79822449d8`)
+      setCharacters(response.data)
+      }
+    catch (error){
+      console.error('error:', error)
+    }
+  }
+
+  if (characters !== ''){     //what other way to do this? why does it console log 4 times?
+  return (                    //why so long to load / why 20 results? / whats deal with class components
+    <div id='container'> 
+      <ul id='list1'>
+        {characters.data.results.map((character)=> 
+        <li key={character.id}> <img src={character.thumbnail.path+'.jpg'} />
+        <p>{character.name}</p></li>
+        )}
+      </ul>
+    </div>
+  )}
+}
+
+export default CharacterList
