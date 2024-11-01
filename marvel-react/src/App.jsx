72c849109642ec695f0e14f1c4e1d09514e50980
DanPaul12 <dan.paul.schechter@gmail.com>
2024-10-31 20:41:41 -0700

    hw51

--- marvel-react/src/App.jsx
+++ marvel-react/src/App.jsx
@@ -7,13 +7,12 @@ function App() {
   const [character, setCharacter] = useState('')
 
   useEffect(() => {
-    if (character === '') {
-    getCharacter('nightcrawler')}
+    getCharacters()
   }, [])
 
-  const getCharacter = async (marvelname) =>{
+  const getCharacters = async () =>{
     try {
-      const response = await axios.get(`https://gateway.marvel.com:443/v1/public/characters?name=${marvelname}&limit=1&ts=1&apikey=eed80ca7412bb4d9fb5325f1f50453d0&hash=3104267868a886eae2f5db79822449d8`)
+      const response = await axios.get(`https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=eed80ca7412bb4d9fb5325f1f50453d0&hash=3104267868a886eae2f5db79822449d8`)
       setCharacter(response.data)
       }
     catch (error){
