a0a2cb27b0f9aef240e93281c559a24e2e02e7dd
DanPaul12 <dan.paul.schechter@gmail.com>
2024-10-31 22:11:52 -0700

    hw51

--- marvel-react/src/App.jsx
+++ marvel-react/src/App.jsx
@@ -4,27 +4,33 @@ import { useState, useEffect } from 'react'
 
 function App() {
 
-  const [character, setCharacter] = useState('')
+  const [characters, setCharacters] = useState('')
 
   useEffect(() => {
     getCharacters()
   }, [])
 
-  const getCharacters = async () =>{
+  const getCharacters = async () => {
     try {
       const response = await axios.get(`https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=eed80ca7412bb4d9fb5325f1f50453d0&hash=3104267868a886eae2f5db79822449d8`)
-      setCharacter(response.data)
+      setCharacters(response.data)
       }
     catch (error){
-      console.error(error)
+      console.error('error:', error)
     }
   }
-  return (
-    <div id='container'>
-      <h3>Name:</h3>
-      {console.log(character)}
+
+  if (characters !== ''){     //what other way to do this? why does it console log 4 times?
+  return (                    //why so long to load
+    <div id='container'> 
+      <ul id='list1'>
+        {characters.data.results.map((character)=> 
+        <li key={character.id}> <img src={character.thumbnail.path+'.jpg'} />
+        <p>{character.name}</p></li>
+        )}
+      </ul>
     </div>
-  )
+  )}
 }
 
 export default App
